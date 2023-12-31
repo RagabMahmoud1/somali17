d64a9c6d92164ff7c6dc589de03a3f44eecf3459
ragabmahmoud1 <ragabdeaf93@outlook.com>
2023-12-31 12:16:12 +0200

    edit

---
+++ addons/rgb_usd_print/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': "RGB USD Account Update",
+    'summary': """
+        summary
+    """,
+    'description': """
+        description
+    """,
+    'author': "Ragab",
+    'contributors': [
+        'Ragab Deaf <ragabdeaf93@outlook.com>',
+    ],
+    'version': '1.0',
+    'depends': ['base', 'account', 'om_account_accountant'],
+    'data': [
+        'report/rgb_format.xml',
+        'views/account_report.xml',
+        'views/report_invoice.xml',
+    ],
+    'license': 'OPL-1',
+    "pre_init_hook": None,
+    "post_init_hook": None,
+}
+# -*- coding: utf-8 -*-
